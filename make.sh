48c54707b64cb9ec7039b36abb7a08c5a5180cf3
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-18 14:43:29 -0500

    remove html version for now

--- make.sh
+++ make.sh
@@ -3,13 +3,9 @@
 # generate html slide show 
 
 pandoc\
- -t revealjs\
  -s\
  -o slides.html\
- README.md\
- -V revealjs-url=https://unpkg.com/reveal.js/\
- --include-in-header=slides.css\
- -V theme=serif
+ README.md
 
 pandoc\
  -t beamer\
